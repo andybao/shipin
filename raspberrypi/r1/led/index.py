e9a10fe005c146ca27cb3fc236ed8730f54aeef9
Andy Bao <wenyu.bao@gmail.com>
2019-12-17 20:39:36 -0500

    add gpio code

--- raspberrypi/r1/led/index.py
+++ raspberrypi/r1/led/index.py
@@ -1,19 +1,26 @@
 import functools
+import RPi.GPIO as GPIO
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
 bp = Blueprint('index', __name__)
+led_pin = 11
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(led_pin, GPIO.OUT)
 
 @bp.route('/', methods=('GET', 'POST'))
 def index():
     if request.method == 'POST':
         if request.form.get('submit_button') == 'ON':
+            GPIO.output(led_pin, GPIO.HIGH)
             print ('ON')
         elif request.form.get('submit_button') == 'OFF':
+            GPIO.output(led_pin, GPIO.LOW)
             print ('OFF')
         elif request.form.get('submit_button') == 'CLEANUP':
+            GPIO.cleanup()
             print ('CLEANUP')
         else:
             pass
